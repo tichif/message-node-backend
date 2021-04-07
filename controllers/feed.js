2089f8db24f555d44d5f67b23b4fb056c0f9ac85
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-04-07 09:21:53 -0400

    Fetch all posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: 1,
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/image.jpg',
+        creator: {
+          name: 'John Doe',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -10,6 +21,6 @@ exports.createPost = (req, res, next) => {
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: { id: new Date().toISOString(), title: title, content: content },
   });
 };
